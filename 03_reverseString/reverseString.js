22fe4e5cf9a4ab94946105fb2d3f97914d9e2a72
Salah A.Fattah <salahfattah1@outlook.com>
2022-04-06 16:55:15 +0200

    Passed all tests

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let stri='';
+    for(let i=str.length-1;i>=0;i--){
+        stri+=str[i];
+    }
+    return stri;
 };
 
 // Do not edit below this line
