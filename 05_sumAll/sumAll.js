40bc6791269e3c1848475b30a4be26e1f8fe02ca
Salah A.Fattah <salahfattah1@outlook.com>
2022-04-06 17:48:55 +0200

    Switch numbers if the bigger is first

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,14 @@
-const sumAll = function() {
-
+const sumAll = function(start,finish) {
+    if(start>finish){
+        let temp=start;
+        start=finish;
+        finish=temp;
+    }
+    let sum=0;
+    for(let i=start;i<=finish;i++){
+        sum+=i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
